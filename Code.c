20ada50a262b04abdb9af0b3c6ec7dc069c9e6ec
Anton-Leonov-IKBO-03-17 <oh22@mail.ru>
2017-09-29 11:47:43 +0300

    Create Code.c

---
+++ Code.c
@@ -0,0 +1,74 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+void convert (double temp, char type) {
+	switch (type) {
+    	case 'C':
+    	case 'c':
+	    	if (temp < -273.15){
+		    	printf("Temperature is below absolute zero, please, try again");
+			    break;
+	        	} else {
+		      	double k = temp + 273.15;
+			      double f = (temp * 9 / 5) + 32;
+		      	printf("%.2f C:\n", temp);
+	     		 printf("%.2f K\n", k);
+		      	printf("%.2f F\n", f);
+			      break;
+	          	}
+
+	case 'K':
+	case 'k':
+		if (temp < 0)
+		{
+			printf("Temperature is below absolute zero, please, try again");
+			break;
+		}
+		else
+		{
+			double c = temp - 273.15;
+			double f = (temp / 5 * 9) - 459.67;
+			printf("%.2f K: \n", temp);
+			printf("%.2f C \n", c);
+			printf("%.2f F \n", f);
+			break;
+		}
+
+	case 'F':
+	case 'f':
+		if (temp < -459.67)
+		{
+			printf("Temperature is below absolute zero, please, try again");
+			break;
+		} else {
+			double c = (temp - 32) * 5 / 9;
+			double k = (temp + 459.67) * 5 / 9;
+			printf("%.2f F: \n", temp);
+			printf("%.2f C \n", c);
+			printf("%.2f K \n", k);
+			break;
+		}
+
+	default:
+		printf("Type anything else!");
+		break;
+	}
+
+}
+
+
+int main(int argc, char *argv[])
+{
+	char type;
+	int a;
+    if (argc == 2) {
+        a = *argv[1];
+        convert(a, 'f');
+        convert(a, 'c');
+        convert(a, 'k');
+    } else {
+        a = *argv[1];
+        type = *argv[2];
+	    convert(a, type);
+    }
+}
